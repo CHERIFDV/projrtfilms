ee4cf74cab540ea85f5331a6351b19dd9220624d
CHERIFDEV <cherifgsair@gmail.com>
2022-05-04 16:08:41 +0100

    last5

--- src/Controller/WatchingController.php
+++ src/Controller/WatchingController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\EpisodeRepository;
+use App\Repository\CommenterRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +13,7 @@ class WatchingController extends AbstractController
     /**
      * @Route("/watching/{id}", name="app_watching")
      */
-    public function index(EpisodeRepository $episodeRepository,int $id): Response
+    public function index(EpisodeRepository $episodeRepository,int $id,CommenterRepository $CommenterRepository): Response
     {
         return $this->render('watching/anime-watching.html.twig', [
             'controller_name' => 'WatchingController','episode' => $episodeRepository->findOneBy(array('deleted_at' => null,"id"=>$id))
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -106,6 +108,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="datetime_immutable", nullable=true)
      */
     private $deleted_at;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -468,6 +475,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setDeletedAt(?\DateTimeImmutable $deleted_at): self
     {
         $this->deleted_at = $deleted_at;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
